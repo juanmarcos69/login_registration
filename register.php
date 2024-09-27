e17a358072026cb2cb3e603cf374538f54766f45
GARCIAJ20 <jovelyngarcia921@gmail.com>
2024-09-27 09:42:00 +0800

    update

--- register.php
+++ register.php
@@ -13,18 +13,29 @@ include('includes/navbar.php');
                         <h5>Login Form</h5>
                     </div>
                     <div class="card-body">
-                        <form action="code.php">
+                        <form action="code.php" method="POST">
                             <div class="form-group mb-3"> 
+                                <label for="name">Name</label>
+                                <input type="text" id="name" name="name" class="form-control">
+                            </div>
+                            <div class="form-group mb-3"> 
+                                <label for="phone">phone Number</label>
+                                <input type="text" id="phone" name="phone" class="form-contrrol"> 
+                            </div>
+                            <div class="form-group mb-3">
                                 <label for="email">Email Address</label>
-                                <input type="email" name="email" class="form-control">
+                                <input type="email" id="email" name="email" class="form-control">
                             </div>
-                            <div class="form-group mb-3"> 
+                            <div class="form-group mb-3">
                                 <label for="password">Password</label>
-                                <input type="password" name="password" class="form-control"> 
+                                <input type="password" id="password" name="password" class="form-control">
+                            </div>  
+                            <div class="form-group mb-3">
+                                <label for="confirm_password">Confirm_Password</label>
+                                <input type="password" id="confirm_password" name="confirm_password" class="form-control">
                             </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Login Now</button> 
-                            </div>
+                            <div class="form-group mb-3">
+                                <button type="submit" name=""
                         </form>
                     </div>
                 </div>
